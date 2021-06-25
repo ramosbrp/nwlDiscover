28a5065715424aae2e0aacb5659bd446cab7ece1
Bruno <bruno.rp45@gmail.com>
2021-06-25 13:21:57 -0300

    Aula 04: Nodemon instalado porém não funcionando corretamente package.json

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
+// Declaração de variáveis
 const express = require('express')
 const route = require ('./route')
 const path = require('path')
 
+
 const server = express()
 
+// Setando ejs
 server.set('view engine', 'ejs')
 
+// Mostrando a pasta public para o express achar os arquivos
+server.use(express.static('public'))
+
+// Mostra o diretório views para o módulo path
 server.set('views', path.join(__dirname, 'views'))
 
 
 server.use(route)
 
+// Porta
 server.listen(3000, ()=> console.log('Rodando'))
 
