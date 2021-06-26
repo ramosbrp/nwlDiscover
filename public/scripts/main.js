a312a2fba924c18b734c3f3407517ff089877fb6
Bruno <bruno.rp45@gmail.com>
2021-06-26 18:09:39 -0300

    Aula 04: button cancelar funcionando

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+import  Modal  from './modal.js'
+
+const modal = Modal()
+
+
+// Pegar todos os botões que existem com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+    // Adicionar a escuta
+    button.addEventListener("click", event=> {
+        // Abrir modal
+        modal.open()
+    })
+})
+
+// Pegar quando o marcar como lido for clicado
+
+
+
